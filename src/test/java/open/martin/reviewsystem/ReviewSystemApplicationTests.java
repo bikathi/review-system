da99b3895c6da03600f0a6b9bdc7ba15702a94d6
Bikathi Martin <bikathimartin@outlook.com>
2023-10-29 20:47:32 +0300

    First commit

---
+++ src/test/java/open/martin/reviewsystem/ReviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package open.martin.reviewsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
