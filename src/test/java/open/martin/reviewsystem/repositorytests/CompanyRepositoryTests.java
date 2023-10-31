36d176babafa1a39c07ba7a35f115119d959e50b
Bikathi Martin <bikathimartin@outlook.com>
2023-10-31 20:04:10 +0300

    Write test to ensure update operation works

--- src/test/java/open/martin/reviewsystem/repositorytests/CompanyRepositoryTests.java
+++ src/test/java/open/martin/reviewsystem/repositorytests/CompanyRepositoryTests.java
@@ -3,10 +3,7 @@ package open.martin.reviewsystem.repositorytests;
 import open.martin.reviewsystem.entity.Company;
 import open.martin.reviewsystem.repository.CompanyRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -61,5 +58,34 @@ public class CompanyRepositoryTests {
         org.junit.jupiter.api.Assertions.assertThrows(Exception.class, () -> {
             companyRepository.save(testCompany);
         });
+    }
+
+    @Test
+    @DisplayName(value = "Test updating an existing Company in DB")
+    void testUpdatingCompany(TestInfo testInfo) {
+        System.out.println("Running test: " + testInfo.getDisplayName());
+
+        // first save a new Company
+        companyRepository.save(testCompany);
+
+        // find the Company in the DB, update something(s) about it and save it again
+        List<Company> companyList = companyRepository.findAll();
+        Company existingCompany = companyList.stream().findFirst().orElse(null);
+        org.junit.jupiter.api.Assertions.assertNotNull(existingCompany); // there should be at least the company we saved
+
+        Long companyId = existingCompany.getEntityId();
+
+        // update sth in the company and save it to the DB
+        existingCompany.setEntityName("Updated entity name");
+        existingCompany.setEntityDescription("Updated entity description");
+        companyRepository.save(existingCompany);
+
+        // read from DB and confirm the changes
+        Company updatedCompany = companyRepository.findById(companyId).orElse(null);
+        org.junit.jupiter.api.Assertions.assertNotNull(updatedCompany);
+
+        Assertions.assertThat(updatedCompany.getEntityId()).isEqualTo(companyId);
+        Assertions.assertThat(updatedCompany.getEntityName()).isEqualTo("Updated entity name");
+        Assertions.assertThat(updatedCompany.getEntityDescription()).isEqualTo("Updated entity description");
     }
 }
