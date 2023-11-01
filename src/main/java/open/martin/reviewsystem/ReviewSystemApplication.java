de065f1fe7c8e60a5ad7c2ff160bdbebc356e52c
Bikathi Martin <bikathimartin@outlook.com>
2023-11-01 21:46:11 +0300

    Add annotation to enable caching

--- src/main/java/open/martin/reviewsystem/ReviewSystemApplication.java
+++ src/main/java/open/martin/reviewsystem/ReviewSystemApplication.java
@@ -2,8 +2,10 @@ package open.martin.reviewsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ReviewSystemApplication {
 
 	public static void main(String[] args) {
