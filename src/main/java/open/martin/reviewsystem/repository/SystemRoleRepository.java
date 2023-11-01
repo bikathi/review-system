6eca9cadbe30f0ca0af9675c9a29a8150c01a07e
Bikathi Martin <bikathimartin@outlook.com>
2023-11-01 23:18:33 +0300

    Add custom method to get role by name

--- src/main/java/open/martin/reviewsystem/repository/SystemRoleRepository.java
+++ src/main/java/open/martin/reviewsystem/repository/SystemRoleRepository.java
@@ -1,9 +1,13 @@
 package open.martin.reviewsystem.repository;
 
 import open.martin.reviewsystem.entity.SystemRole;
+import open.martin.reviewsystem.type.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SystemRoleRepository extends JpaRepository<SystemRole, Integer> {
+    Optional<SystemRole> findByName(Role role);
 }
