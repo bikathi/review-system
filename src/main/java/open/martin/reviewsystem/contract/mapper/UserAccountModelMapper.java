7633a9763d600c8799ca20f9f8c77652d3a9f783
Bikathi Martin <bikathimartin@outlook.com>
2023-11-02 15:28:03 +0300

    Change package of application contracts

--- src/main/java/open/martin/reviewsystem/contract/mapper/UserAccountModelMapper.java
+++ src/main/java/open/martin/reviewsystem/contract/mapper/UserAccountModelMapper.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 public abstract class UserAccountModelMapper {
     @Autowired
