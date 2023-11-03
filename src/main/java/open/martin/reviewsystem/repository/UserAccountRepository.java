be68e9c13dff0e8edbc06f843080d18302d62e09
Bikathi Martin <bikathimartin@outlook.com>
2023-11-03 21:58:19 +0300

    Add custom method to find account by username

--- src/main/java/open/martin/reviewsystem/repository/UserAccountRepository.java
+++ src/main/java/open/martin/reviewsystem/repository/UserAccountRepository.java
@@ -4,6 +4,9 @@ import open.martin.reviewsystem.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    Optional<UserAccount> findByUsername(String username);
 }
