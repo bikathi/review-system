55edac2d32d8fd4a4a658d3ec771e4b399e43233
Bikathi Martin <bikathimartin@outlook.com>
2023-11-04 23:07:49 +0300

    Add new advices for security-based errors

--- src/main/java/open/martin/reviewsystem/exception/ControllerExceptionHandler.java
+++ src/main/java/open/martin/reviewsystem/exception/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package open.martin.reviewsystem.exception;
 import open.martin.reviewsystem.payload.ControllerExceptionPayload;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -19,5 +20,25 @@ public class ControllerExceptionHandler {
             ex.getMessage(),
             request.getDescription(true));
         return ResponseEntity.badRequest().body(payload);
+    }
+
+    @ExceptionHandler(value = JWTVerificationException.class)
+    public final ResponseEntity<ControllerExceptionPayload> authenticationException(JWTVerificationException ex, WebRequest request) {
+        ControllerExceptionPayload payload = new ControllerExceptionPayload(
+            HttpStatus.UNAUTHORIZED.value(),
+            new Date(),
+            ex.getMessage(),
+            request.getDescription(true));
+        return new ResponseEntity<>(payload, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    public final ResponseEntity<ControllerExceptionPayload> methodSecurityException(Exception ex, WebRequest request) {
+        ControllerExceptionPayload payload = new ControllerExceptionPayload(
+            HttpStatus.FORBIDDEN.value(),
+            new Date(),
+            "Insufficient permissions to perform this action",
+            request.getDescription(true));
+        return new ResponseEntity<>(payload, HttpStatus.FORBIDDEN);
     }
 }
