4ab9cf3a27b0315c3569a5eb8d713d9ceadec1c1
Bikathi Martin <bikathimartin@outlook.com>
2023-11-05 12:03:38 +0300

    Add contract method to refresh auth token

--- src/main/java/open/martin/reviewsystem/contract/controller/AuthenticationContract.java
+++ src/main/java/open/martin/reviewsystem/contract/controller/AuthenticationContract.java
@@ -3,5 +3,6 @@ package open.martin.reviewsystem.contract.controller;
 import org.springframework.http.ResponseEntity;
 
 public interface AuthenticationContract {
-    public ResponseEntity<?> login();
+    ResponseEntity<?> login();
+    ResponseEntity<?> refreshAuthToken();
 }
--- src/main/java/open/martin/reviewsystem/controller/AuthenticationController.java
+++ src/main/java/open/martin/reviewsystem/controller/AuthenticationController.java
@@ -2,17 +2,28 @@ package open.martin.reviewsystem.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import open.martin.reviewsystem.contract.controller.AuthenticationContract;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/v1/user-account")
+@RequestMapping(value = "/api/v1/auth")
 @Slf4j
 public final class AuthenticationController implements AuthenticationContract {
+    @Override
+    @ResponseStatus(code = HttpStatus.OK)
+    @PostMapping(value = "/login")
+    public ResponseEntity<?> login() {
+        return null;
+    }
 
     @Override
-    public ResponseEntity<?> login() {
+    @ResponseStatus(code = HttpStatus.OK)
+    @PostMapping(value = "/refresh-auth")
+    public ResponseEntity<?> refreshAuthToken() {
         return null;
     }
 }
