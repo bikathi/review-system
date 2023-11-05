cc0038ab444ead6509db2e7bdec68594c9f82cff
Bikathi Martin <bikathimartin@outlook.com>
2023-11-05 16:57:48 +0300

    Add exception handler for *notFound exceptions

--- src/main/java/open/martin/reviewsystem/exception/ControllerExceptionHandler.java
+++ src/main/java/open/martin/reviewsystem/exception/ControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import open.martin.reviewsystem.payload.ControllerExceptionPayload;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -40,5 +41,15 @@ public class ControllerExceptionHandler {
             "Insufficient permissions to perform this action",
             request.getDescription(true));
         return new ResponseEntity<>(payload, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(value = { ResourceNotFoundException.class, UsernameNotFoundException.class })
+    public final ResponseEntity<ControllerExceptionPayload> notFoundException(RuntimeException ex, WebRequest request) {
+        ControllerExceptionPayload payload = new ControllerExceptionPayload(
+            HttpStatus.FORBIDDEN.value(),
+            new Date(),
+            ex.getMessage(),
+            request.getDescription(true));
+        return new ResponseEntity<>(payload, HttpStatus.NOT_FOUND);
     }
 }
