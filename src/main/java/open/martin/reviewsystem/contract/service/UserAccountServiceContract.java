e7d055b1290054836bb152ce67ac9813ac1fa265
Bikathi Martin <bikathimartin@outlook.com>
2023-11-05 13:02:48 +0300

    Complete wiring up of user account service functionality

--- src/main/java/open/martin/reviewsystem/contract/service/UserAccountServiceContract.java
+++ src/main/java/open/martin/reviewsystem/contract/service/UserAccountServiceContract.java
@@ -1,9 +1,11 @@
 package open.martin.reviewsystem.contract.service;
 
+import open.martin.reviewsystem.entity.UserAccount;
+
 public interface UserAccountServiceContract {
-    void createUserAccount();
-    void updateUserAccountDetails();
-    void getUserAccountById();
-    void deleteUserAccountById();
-    void deleteUserAccountByName();
+    UserAccount createUserAccount(UserAccount userAccount);
+    void updateUserAccountDetails(UserAccount userAccount);
+    UserAccount getUserAccountById(Long accountId);
+    void deleteUserAccountById(Long accountId);
+    void deleteUserAccountByUsername(String username);
 }
--- src/main/java/open/martin/reviewsystem/service/UserAccountService.java
+++ src/main/java/open/martin/reviewsystem/service/UserAccountService.java
@@ -1,7 +1,9 @@
 package open.martin.reviewsystem.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import open.martin.reviewsystem.contract.service.UserAccountServiceContract;
+import open.martin.reviewsystem.entity.UserAccount;
 import open.martin.reviewsystem.repository.UserAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,27 +15,38 @@ public class UserAccountService implements UserAccountServiceContract {
     private UserAccountRepository userAccountRepository;
 
     @Override
-    public void createUserAccount() {
-
+    public UserAccount createUserAccount(UserAccount userAccount) {
+        return userAccountRepository.save(userAccount);
     }
 
     @Override
-    public void updateUserAccountDetails() {
+    public void updateUserAccountDetails(UserAccount userAccount) throws EntityNotFoundException {
+        UserAccount existingUserAccount = userAccountRepository.findById(userAccount.getAccountId()).orElseThrow(
+            () -> new EntityNotFoundException("Account with accountId " + userAccount.getAccountId() + " not found")
+        );
 
+        // TODO: update the user account with necessary details
+
+        // save the user
+        userAccountRepository.save(existingUserAccount);
     }
 
     @Override
-    public void getUserAccountById() {
+    public UserAccount getUserAccountById(Long accountId) throws EntityNotFoundException {
+        UserAccount existingUserAccount = userAccountRepository.findById(accountId).orElseThrow(
+            () -> new EntityNotFoundException("User of accountId " + accountId + " not found")
+        );
 
+        return existingUserAccount;
     }
 
     @Override
-    public void deleteUserAccountById() {
-
+    public void deleteUserAccountById(Long accountId) {
+        userAccountRepository.deleteById(accountId);
     }
 
     @Override
-    public void deleteUserAccountByName() {
-
+    public void deleteUserAccountByUsername(String username) {
+        userAccountRepository.deleteByUsername(username);
     }
 }
